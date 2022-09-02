1f143b054d64eb8f1f0ae03f981463bc34a74b43
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2022-09-02 10:36:20 +0400

    feat(components): add button

--- src/app/common/components/components.module.ts
+++ src/app/common/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarModule } from './calendar/calendar.module';
+import { ButtonModule } from './button/button.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [CalendarModule],
+  exports: [CalendarModule, ButtonModule],
 })
 export class ComponentsModule {}
